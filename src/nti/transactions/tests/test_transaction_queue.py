2f2d833e7737fb01c252e80d0202557954784d9a
Jason Madden <jamadden@gmail.com>
2016-07-27 07:15:50 -0500

    untabify

--- src/nti/transactions/tests/test_transaction_queue.py
+++ src/nti/transactions/tests/test_transaction_queue.py
@@ -14,11 +14,11 @@ from hamcrest import assert_that
 import transaction
 
 try:
-	from gevent.queue import Full
-	from gevent.queue import Queue
+        from gevent.queue import Full
+        from gevent.queue import Queue
 except ImportError:
-	from Queue import Full
-	from Queue import Queue
+        from Queue import Full
+        from Queue import Queue
 
 from nti.transactions.transactions import put_nowait
 
@@ -26,81 +26,81 @@ from nti.testing.base import AbstractTestBase
 
 class PutQueueTest(AbstractTestBase):
 
-	def test_put_succeeds(self):
-		queue = Queue() # unbounded
-		transaction.begin()
+        def test_put_succeeds(self):
+                queue = Queue() # unbounded
+                transaction.begin()
 
-		put_nowait( queue, self )
-		# still empty
-		assert_that( queue.qsize(), is_( 0 ) )
+                put_nowait( queue, self )
+                # still empty
+                assert_that( queue.qsize(), is_( 0 ) )
 
-		transaction.commit()
+                transaction.commit()
 
-		assert_that( queue.get(block=False), is_( self ) )
+                assert_that( queue.get(block=False), is_( self ) )
 
-	def test_put_transaction_abort(self):
-		queue = Queue()
-		transaction.begin()
-		put_nowait( queue, 'aborted' )
-		transaction.abort()
+        def test_put_transaction_abort(self):
+                queue = Queue()
+                transaction.begin()
+                put_nowait( queue, 'aborted' )
+                transaction.abort()
 
-		transaction.begin()
-		put_nowait( queue, 'committed' )
-		transaction.commit()
+                transaction.begin()
+                put_nowait( queue, 'committed' )
+                transaction.commit()
 
-		assert_that( queue.qsize(), is_( 1 ) )
-		assert_that( queue.get( block=False ), is_( 'committed' ) )
+                assert_that( queue.qsize(), is_( 1 ) )
+                assert_that( queue.get( block=False ), is_( 'committed' ) )
 
-	def test_put_transaction_savepoint(self):
-		queue = Queue()
-		transaction.begin()
-		put_nowait( queue, 'presavepoint' )
-		# we can get a non-optimistic savepoint
-		savepoint = transaction.savepoint(optimistic=False)
-		assert_that( savepoint._savepoints, has_length( 1 ) )
-		repr(savepoint._savepoints) # cover
-		put_nowait( queue, 'aftersavepoint' )
+        def test_put_transaction_savepoint(self):
+                queue = Queue()
+                transaction.begin()
+                put_nowait( queue, 'presavepoint' )
+                # we can get a non-optimistic savepoint
+                savepoint = transaction.savepoint(optimistic=False)
+                assert_that( savepoint._savepoints, has_length( 1 ) )
+                repr(savepoint._savepoints) # cover
+                put_nowait( queue, 'aftersavepoint' )
 
-		# If we rollback the savepoint now, what we just
-		# did will be lost, but the original work
-		# will still happen
-		savepoint.rollback()
-		transaction.commit()
+                # If we rollback the savepoint now, what we just
+                # did will be lost, but the original work
+                # will still happen
+                savepoint.rollback()
+                transaction.commit()
 
-		assert_that( queue.qsize(), is_( 1 ) )
-		assert_that( queue.get( block=False ), is_( 'presavepoint' ) )
+                assert_that( queue.qsize(), is_( 1 ) )
+                assert_that( queue.get( block=False ), is_( 'presavepoint' ) )
 
-	def test_put_multiple_correct_order(self):
-		# Early builds had a bug where the sort order of the datamanagers
-		# was non-deterministic since it was based on object id, and that's not
-		# guaranteed to be atomically increasing. It takes a high iteration count to
-		# demonstrate this, though
-		queue = Queue()
+        def test_put_multiple_correct_order(self):
+                # Early builds had a bug where the sort order of the datamanagers
+                # was non-deterministic since it was based on object id, and that's not
+                # guaranteed to be atomically increasing. It takes a high iteration count to
+                # demonstrate this, though
+                queue = Queue()
 
-		for _ in range(10000):
-			transaction.begin()
+                for _ in range(10000):
+                        transaction.begin()
 
-			put_nowait( queue, 'a' )
-			put_nowait( queue, 'b' )
+                        put_nowait( queue, 'a' )
+                        put_nowait( queue, 'b' )
 
-			transaction.commit()
+                        transaction.commit()
 
-			assert_that( queue.get( block=False ), is_( 'a' ) )
-			assert_that( queue.get( block=False ), is_( 'b' ) )
+                        assert_that( queue.get( block=False ), is_( 'a' ) )
+                        assert_that( queue.get( block=False ), is_( 'b' ) )
 
-	def test_put_failure( self ):
-		queue = Queue(1) # unbounded
-		queue.put( object() )
-		assert_that( queue.qsize(), is_( 1 ) )
+        def test_put_failure( self ):
+                queue = Queue(1) # unbounded
+                queue.put( object() )
+                assert_that( queue.qsize(), is_( 1 ) )
 
-		transaction.begin()
+                transaction.begin()
 
-		put_nowait( queue, self )
-		# still size 1
-		assert_that( queue.qsize(), is_( 1 ) )
-		with self.assertRaises( Full ) as cm:
-			transaction.commit()
+                put_nowait( queue, self )
+                # still size 1
+                assert_that( queue.qsize(), is_( 1 ) )
+                with self.assertRaises( Full ) as cm:
+                        transaction.commit()
 
 
-		assert_that( cm.exception, is_( Full ) )
-		assert_that( queue.get(block=False), is_( object ) )
+                assert_that( cm.exception, is_( Full ) )
+                assert_that( queue.get(block=False), is_( object ) )
