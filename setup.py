c6ed3ae9a1f7038c1dbf9c24f8ff2d2f038d7c62
Jason Madden <jamadden@gmail.com>
2016-07-27 07:29:25 -0500

    fix tab width

--- setup.py
+++ setup.py
@@ -4,56 +4,56 @@ from setuptools import setup, find_packages
 VERSION = '0.0.0'
 
 entry_points = {
-        'console_scripts': [
-        ],
+    'console_scripts': [
+    ],
 }
 
 TESTS_REQUIRE = [
-        'nose',
-        'nose-timer',
-        'nose-pudb',
-        'nose-progressive',
-        'nose2[coverage_plugin]',
-        'pyhamcrest',
-        'nti.nose_traceback_info',
-        'nti.testing'
+    'nose',
+    'nose-timer',
+    'nose-pudb',
+    'nose-progressive',
+    'nose2[coverage_plugin]',
+    'pyhamcrest',
+    'nti.nose_traceback_info',
+    'nti.testing'
 ]
 
 setup(
-        name='nti.transactions',
-        version=VERSION,
-        author='Jason Madden',
-        author_email='jason@nextthought.com',
-        description="NTI Transactions Utility",
-        long_description=codecs.open('README.rst', encoding='utf-8').read(),
-        license='Proprietary',
-        keywords='ZODB Transactions',
-        classifiers=[
-                'Intended Audience :: Developers',
-                'Natural Language :: English',
-                'Operating System :: OS Independent',
-                'Programming Language :: Python :: 2',
-                'Programming Language :: Python :: 2.7',
-                'Programming Language :: Python :: Implementation :: CPython'
-        ],
-        packages=find_packages('src'),
-        package_dir={'': 'src'},
-        namespace_packages=['nti'],
-        tests_require=TESTS_REQUIRE,
-        install_requires=[
-                'setuptools',
-                'dm.transaction.aborthook',
-                'gevent',
-                'perfmetrics',
-                'transaction',
-                'ZODB',
-                'zope.component',
-                'zope.exceptions',
-                'zope.interface',
-                'zope.security'
-        ],
-        extras_require={
-                'test': TESTS_REQUIRE,
-        },
-        entry_points=entry_points
+    name='nti.transactions',
+    version=VERSION,
+    author='Jason Madden',
+    author_email='jason@nextthought.com',
+    description="NTI Transactions Utility",
+    long_description=codecs.open('README.rst', encoding='utf-8').read(),
+    license='Proprietary',
+    keywords='ZODB Transactions',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: Implementation :: CPython'
+    ],
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    namespace_packages=['nti'],
+    tests_require=TESTS_REQUIRE,
+    install_requires=[
+        'setuptools',
+        'dm.transaction.aborthook',
+        'gevent',
+        'perfmetrics',
+        'transaction',
+        'ZODB',
+        'zope.component',
+        'zope.exceptions',
+        'zope.interface',
+        'zope.security'
+    ],
+    extras_require={
+        'test': TESTS_REQUIRE,
+    },
+    entry_points=entry_points
 )
